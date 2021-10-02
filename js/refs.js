ab0869965b2520d9cffa2c4f727b4eb6e0d3570d
volokads <hluhovspg@gmail.com>
2021-10-02 12:16:52 +0300

    repaired

--- js/refs.js
+++ js/refs.js
@@ -3,5 +3,6 @@ export default {
     daysCounter: document.querySelector('span[data-value="days"]'),
     hoursCounter: document.querySelector('span[data-value="hours"]'),
     minsCounter: document.querySelector('span[data-value="mins"]'),
-    secsCounter: document.querySelector('span[data-value="secs"]')
+    secsCounter: document.querySelector('span[data-value="secs"]'),
 }
+
